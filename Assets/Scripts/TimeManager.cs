3e77d124a92c8210c2053ae128879092256f2dd4
Sh4dow <killymc35@gmail.com>
2026-01-30 20:58:21 +0000

    FEATURE: Display Text Updates Real Time

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,12 +1,19 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    public float hoursRemaining = 24;
+    public int hoursRemaining = 24;
+    public TextMeshProUGUI timeText;
 
-    public void SpendTime(float time)
+    public void SpendTime(int time)
     {
         hoursRemaining -= time;
+    }
+
+    private void Update()
+    {
+        timeText.text = hoursRemaining.ToString("00");
     }
 }
