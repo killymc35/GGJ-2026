936a71e0f53b8ec8fe70e2bb87691d3d04dfe1ec
Sh4dow <killymc35@gmail.com>
2026-01-30 19:46:19 +0000

    FEATURE: Zoom In

--- Assets/Scripts/InteractableObjects/BoardItem.cs
+++ Assets/Scripts/InteractableObjects/BoardItem.cs
@@ -1,10 +1,13 @@
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class BoardItem : MonoBehaviour, InteractableObject
 {
     public void Interact()
     {
+        gameObject.GetComponentInChildren<CinemachineCamera>().Priority = 2;
+        
         Debug.Log("Interaction Registered");
     }
 }
