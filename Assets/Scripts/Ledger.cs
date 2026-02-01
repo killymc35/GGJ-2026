8a46f2c2d908b4d9ac2c33827740eda618919c00
Sh4dow <killymc35@gmail.com>
2026-02-01 12:34:15 +0000

    Revert "FEATURE: Log updates with Facts"
    This reverts commit ba1eedad0fcb21aad006cb79c516bfc8662e037c.

--- Assets/Scripts/Ledger.cs
+++ Assets/Scripts/Ledger.cs
@@ -20,16 +20,12 @@ public class Ledger : MonoBehaviour
     public int correctWhoGuess;
     public int correctWhereGuess;
     public int correctWhenGuess;
-    
-    public int factCounter = 0;
-    public TextMeshProUGUI[] facts;
 
     public void GiveInfo(BoardItem.Clue clue)
     {
         switch (clue.type)
         {
             case BoardItem.Type.Fact:
-                LogFact(clue.description);
                 break;
             case BoardItem.Type.Who:
                 break;
@@ -38,12 +34,6 @@ public class Ledger : MonoBehaviour
             case BoardItem.Type.Where:
                 break;
         }
-    }
-    
-    public void LogFact(string fact)
-    {
-        facts[factCounter].text = "•  " + fact;
-        factCounter++;
     }
 
     public struct Character : IEquatable<Character>
